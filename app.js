5f19d45a07d754e226707bb51a97625ef93806f0
miguelmballeza <miguelballeza53@aragon.unam.mx>
2021-05-17 22:43:25 -0500

    Me parece que esta bien, pero la verdad no entiendo por que no pasa del primer condicional de test.js, intente borrando ese condicional y nadamas ejecutando el de filtrarTareasPorEstado() y me corrio correctamente, necesito ver los detalles más a fondo

--- app.js
+++ app.js
@@ -8,28 +8,35 @@
 */
 const fs = require('fs');
 const path = './tareas.json';
-exports.leerTareas = function () {
-    return fs.readFileSync(path, 'utf-8');
+leerTareas = function () {
+    return fs.readFileSync(path, "utf8");
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
 }
 
-exports.agregarTarea = function (object) {
-    console.log(JSON.stringify(object));
-    let arregloJSON = JSON.parse(fs.readFileSync(path, 'utf-8').toString());
-    console.log('Arreglo : ' + arregloJSON.length);
-    let resultado = arregloJSON.push(JSON.stringify(object));
-    fs.writeFileSync(path, resultado);
+agregarTarea = function (object) {
+    let arregloJSON = JSON.parse(fs.readFileSync(path, 'utf-8'));
+    arregloJSON.push(JSON.parse(JSON.stringify(object)));
+    fs.writeFileSync(path, JSON.stringify(arregloJSON));
+    let valor = this.leerTareas();
+    // console.log(!valor==2);
+    // console.log('Tiene que ser False : ' + !valor.length);
     /*
         Registra y guarda una nueva tarea.
     */
 }
 
-exports.filtrarTareasPorEstado = function (estado) {
-    return JSON.parse(leerTareas()).filter( elemento => elemento.status === estado );
+filtrarTareasPorEstado = function (estado) {
+    return JSON.parse(this.leerTareas()).filter( elemento => elemento.status === estado );
     /*
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
 }
+
+module.exports = {
+    leerTareas,
+    agregarTarea,
+    filtrarTareasPorEstado
+};
