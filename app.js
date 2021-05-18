fcebec8ac575a2f45e723460c3cd39de99a7d626
miguelmballeza <miguelballeza53@aragon.unam.mx>
2021-05-17 23:12:33 -0500

    Funcionando

--- app.js
+++ app.js
@@ -9,30 +9,18 @@
 const fs = require('fs');
 const path = './tareas.json';
 leerTareas = function () {
-    return fs.readFileSync(path, "utf8");
-    /*
-        En esta función debes leer y retornar las tareas registradas.
-    */
+    let objeto = fs.readFileSync(path, "utf8");
+    return JSON.parse(objeto);
 }
 
 agregarTarea = function (object) {
-    let arregloJSON = JSON.parse(fs.readFileSync(path, 'utf-8'));
-    arregloJSON.push(JSON.parse(JSON.stringify(object)));
+    let arregloJSON = JSON.parse(fs.readFileSync(path, 'utf-8').toString());
+    arregloJSON.push(object);
     fs.writeFileSync(path, JSON.stringify(arregloJSON));
-    let valor = this.leerTareas();
-    // console.log(!valor==2);
-    // console.log('Tiene que ser False : ' + !valor.length);
-    /*
-        Registra y guarda una nueva tarea.
-    */
 }
 
 filtrarTareasPorEstado = function (estado) {
-    return JSON.parse(this.leerTareas()).filter( elemento => elemento.status === estado );
-    /*
-        En esta función debes de leer las tareas y retornar las que tengan el estado que se
-        manda en el parametro.
-    */
+    return (this.leerTareas()).filter( elemento => elemento.status === estado );
 }
 
 module.exports = {
