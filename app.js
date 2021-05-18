7aa1db53dd6456be07871885a992f5d2bb693b8f
miguelmballeza <miguelballeza53@aragon.unam.mx>
2021-05-17 21:28:04 -0500

    Avances hasta menos de las 9:30 del día 17 de Mayo del 2021

--- app.js
+++ app.js
@@ -6,20 +6,28 @@
            * pendiente
          )
 */
-
+const fs = require('fs');
+const path = './tareas.json';
 exports.leerTareas = function () {
+    return fs.readFileSync(path, 'utf-8');
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
 }
 
-exports.agregarTarea = function (tarea) {
+exports.agregarTarea = function (object) {
+    console.log(JSON.stringify(object));
+    let arregloJSON = JSON.parse(fs.readFileSync(path, 'utf-8').toString());
+    console.log('Arreglo : ' + arregloJSON.length);
+    let resultado = arregloJSON.push(JSON.stringify(object));
+    fs.writeFileSync(path, resultado);
     /*
         Registra y guarda una nueva tarea.
     */
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
+    return JSON.parse(leerTareas()).filter( elemento => elemento.status === estado );
     /*
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
